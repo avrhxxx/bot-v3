7e3ae7b910b34f3b03a225834728c0406ec91d28
avrhxxx <stelczyk0797@gmail.com>
2026-02-21 14:04:05 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import { config } from "./config/config";
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`Bot zalogowany jako ${client.user?.tag}`);
+});
+
+client.login(config.token);
