fc02e2ef9c1934c92e8f518e5a0215942552aece
avrhxxx <stelczyk0797@gmail.com>
2026-02-21 14:39:26 +0100

    Update AllianceTypes.ts

--- src/features/alliance/AllianceTypes.ts
+++ src/features/alliance/AllianceTypes.ts
@@ -1,1 +1,12 @@
-1
+export interface Alliance {
+  id: string;
+  name: string;
+
+  r5: string;          // leader
+  r4: string[];        // moderators
+  r3: string[];        // members
+
+  identityRoleId?: string;
+
+  createdAt: number;
+}
