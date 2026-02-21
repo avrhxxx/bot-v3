d882aeab3beda9650d6e3fd20883a3eb02306ec4
avrhxxx <stelczyk0797@gmail.com>
2026-02-21 14:24:31 +0100

    Update Database.ts

--- src/data/Database.ts
+++ src/data/Database.ts
@@ -1,1 +1,19 @@
-1
+type Collection<T> = Map<string, T>;
+
+export class Database {
+  private static instance: Database;
+
+  public alliances: Collection<any> = new Map();
+  public journal: Collection<any> = new Map();
+  public ownership: Collection<any> = new Map();
+  public health: Collection<any> = new Map();
+
+  private constructor() {}
+
+  public static getInstance(): Database {
+    if (!Database.instance) {
+      Database.instance = new Database();
+    }
+    return Database.instance;
+  }
+}
