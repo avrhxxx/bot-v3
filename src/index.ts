de57fbab63134f03d35deeab22a9a3af797923d7
avrhxxx <stelczyk0797@gmail.com>
2026-02-21 22:38:10 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,35 @@
-console.log("THIS IS NEW BUILD 123");
+console.log("THIS IS NEW BUILD 124");
 
 import { IntegrityMonitor } from "./system/snapshot/IntegrityMonitor";
 import { Health } from "./system/Health";
 import { startDiscord } from "./discord/client";
+import { SnapshotService } from "./system/snapshot/SnapshotService";
+import { SafeMode } from "./system/SafeMode";
 
-console.log("System booting...");
+async function bootstrap() {
+  console.log("System booting...");
 
-Health.setHealthy();
+  const corrupted = SnapshotService.verifyAll();
 
-IntegrityMonitor.start(15000);
+  if (corrupted.length === 0) {
+    Health.setHealthy();
+    console.log("Initial integrity check passed.");
+  } else {
+    Health.setCritical(
+      `Boot integrity failure detected in ${corrupted.length} alliance(s)`
+    );
+    SafeMode.activate("Boot integrity failure");
+    console.log("Boot integrity failure. SafeMode activated.");
+  }
 
-startDiscord();
+  IntegrityMonitor.start(15000);
 
-console.log("Integrity Monitor started.");
+  await startDiscord();
+
+  console.log("Integrity Monitor started.");
+}
+
+bootstrap().catch((err) => {
+  console.error("Fatal boot error:", err);
+  process.exit(1);
+});
