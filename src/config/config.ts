e062afa6ad02b04836dd62feed560bf962346b1e
avrhxxx <stelczyk0797@gmail.com>
2026-02-21 16:35:00 +0100

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,5 @@
 export const config = {
-  token: process.env.BOT_TOKEN as string
+  token: process.env.BOT_TOKEN as string,
+  clientId: process.env.CLIENT_ID as string,
+  guildId: process.env.GUILD_ID as string
 };
