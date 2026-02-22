a2d883a1718f9b277d92bdccf914648c1b7a4c67
avrhxxx <stelczyk0797@gmail.com>
2026-02-22 13:05:22 +0100

    Update CommandRegistry.ts

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -1,8 +1,14 @@
 import { Collection } from "discord.js";
 import { Command } from "./Command";
+import { SysSetupCommand } from "./sys/setup"; // importujemy komendę setup
 
 export class CommandRegistry {
   private commands = new Collection<string, Command>();
+
+  constructor() {
+    // Rejestracja komendy /x sys setup od razu przy tworzeniu registry
+    this.register(SysSetupCommand as unknown as Command); // cast do Command, jeśli potrzebny
+  }
 
   register(command: Command) {
     this.commands.set(command.data.name, command);
