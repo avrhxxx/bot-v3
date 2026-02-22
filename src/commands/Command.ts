feea6d18b75ff4ecdb4995d14051ea1585e7cbbd
avrhxxx <stelczyk0797@gmail.com>
2026-02-22 12:23:13 +0100

    Update Command.ts

--- src/commands/Command.ts
+++ src/commands/Command.ts
@@ -3,4 +3,9 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 export interface Command {
   data: SlashCommandBuilder;
   execute(interaction: ChatInputCommandInteraction): Promise<void>;
+
+  /**
+   * If true, only BotOwner can execute.
+   */
+  ownerOnly?: boolean;
 }
