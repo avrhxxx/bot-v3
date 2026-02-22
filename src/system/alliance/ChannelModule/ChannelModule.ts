807c1367d7b318dd31d8a60ae3871f611937125c
avrhxxx <stelczyk0797@gmail.com>
2026-02-22 19:13:01 +0100

    Update ChannelModule.ts

--- src/system/alliance/ChannelModule/ChannelModule.ts
+++ src/system/alliance/ChannelModule/ChannelModule.ts
@@ -10,20 +10,12 @@ export interface AllianceChannels {
 }
 
 export class ChannelModule {
-  /**
-   * Tworzy strukturę kanałów sojuszu i ustawia uprawnienia dla ról
-   */
   static async createChannels(guild: Guild, tag: string, roles: AllianceRoles): Promise<AllianceChannels> {
     const createdChannels: (CategoryChannel | TextChannel)[] = [];
 
-    // Kategoria
-    const category = await guild.channels.create({
-      name: `Alliance • ${tag}`,
-      type: ChannelType.GuildCategory
-    }) as CategoryChannel;
+    const category = await guild.channels.create({ name: `Alliance • ${tag}`, type: ChannelType.GuildCategory }) as CategoryChannel;
     createdChannels.push(category);
 
-    // Kanały
     const leadership = await guild.channels.create({ name: "leadership", type: ChannelType.GuildText, parent: category.id }) as TextChannel;
     const officers = await guild.channels.create({ name: "officers", type: ChannelType.GuildText, parent: category.id }) as TextChannel;
     const members = await guild.channels.create({ name: "members", type: ChannelType.GuildText, parent: category.id }) as TextChannel;
@@ -31,30 +23,12 @@ export class ChannelModule {
 
     createdChannels.push(leadership, officers, members, join);
 
-    // Uprawnienia
     const everyoneId = guild.roles.everyone.id;
 
-    await leadership.permissionOverwrites.set([
-      { id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] }
-    ]);
-
-    await officers.permissionOverwrites.set([
-      { id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r4RoleId, allow: [PermissionFlagsBits.ViewChannel] }
-    ]);
-
-    await members.permissionOverwrites.set([
-      { id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r4RoleId, allow: [PermissionFlagsBits.ViewChannel] },
-      { id: roles.r3RoleId, allow: [PermissionFlagsBits.ViewChannel] }
-    ]);
-
-    await join.permissionOverwrites.set([
-      { id: everyoneId, allow: [PermissionFlagsBits.ViewChannel] }
-    ]);
+    await leadership.permissionOverwrites.set([{ id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] }, { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] }]);
+    await officers.permissionOverwrites.set([{ id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] }, { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] }, { id: roles.r4RoleId, allow: [PermissionFlagsBits.ViewChannel] }]);
+    await members.permissionOverwrites.set([{ id: everyoneId, deny: [PermissionFlagsBits.ViewChannel] }, { id: roles.r5RoleId, allow: [PermissionFlagsBits.ViewChannel] }, { id: roles.r4RoleId, allow: [PermissionFlagsBits.ViewChannel] }, { id: roles.r3RoleId, allow: [PermissionFlagsBits.ViewChannel] }]);
+    await join.permissionOverwrites.set([{ id: everyoneId, allow: [PermissionFlagsBits.ViewChannel] }]);
 
     return {
       categoryId: category.id,
