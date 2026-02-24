8020541ae20dafd8b2f1475ecbb76a3c03fe9d86
avrhxxx <stelczyk0797@gmail.com>
2026-02-24 20:02:38 +0100

    Create broadcast.ts

---
+++ src/commands/alliance/broadcast.ts
@@ -0,0 +1,10 @@
+// ============================================
+// COMMAND: broadcast
+// FILE: src/commands/alliance/broadcast.ts
+// ============================================
+
+// TODO: implement logic to send broadcast message to all alliance members via BroadcastModule
+
+export async function execute() {
+  // TODO: implement
+}
