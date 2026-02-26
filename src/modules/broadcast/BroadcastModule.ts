9421bf586da8ceb3009fb4484c080734f559ca19
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 21:37:48 +0100

    Update BroadcastModule.ts

--- src/modules/broadcast/BroadcastModule.ts
+++ src/modules/broadcast/BroadcastModule.ts
@@ -1,9 +1,6 @@
+// src/modules/broadcast/BroadcastModule.ts
 export class BroadcastModule {
   static notifyJoin(allianceId: string, memberId: string) {
-    console.log(`[Broadcast] Member ${memberId} joined ${allianceId}`);
-  }
-
-  static notifyLeaderChange(allianceId: string, newLeaderId: string) {
-    console.log(`[Broadcast] Leader changed to ${newLeaderId} in ${allianceId}`);
+    console.log(`[BroadcastModule] notifyJoin ${memberId} in ${allianceId}`);
   }
 }
