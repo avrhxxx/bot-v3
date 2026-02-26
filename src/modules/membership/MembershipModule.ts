7ba44b9d589dd75dcdd151cb5db28794c156a2ad
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 21:28:49 +0100

    Create MembershipModule.ts

---
+++ src/modules/membership/MembershipModule.ts
@@ -0,0 +1,16 @@
+import { RulesModule } from "../rules/RulesModule";
+import { BroadcastModule } from "../broadcast/BroadcastModule";
+import { RoleModule } from "../role/RoleModule";
+import { AllianceService } from "../../AllianceService";
+import { MutationGate } from "../../engine/MutationGate";
+
+export class MembershipModule {
+  static async addMember(allianceId: string, memberId: string) {
+    await MutationGate.execute({ actor: memberId, operation: 'ADD_MEMBER', allianceId }, async () => {
+      RulesModule.validateJoin(allianceId, memberId);
+      AllianceService.addMember(allianceId, memberId);
+      await RoleModule.assignMemberRoles(allianceId, memberId);
+      BroadcastModule.notifyJoin(allianceId, memberId);
+    });
+  }
+}
