c39d61f138d9efd52ed79aafb728b357365d0593
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 23:57:44 +0100

    Update RulesModule.ts

--- src/modules/rules/RulesModule.ts
+++ src/modules/rules/RulesModule.ts
@@ -1,11 +1,26 @@
-import { AllianceService } from "../../AllianceServices";
+// src/modules/rules/RulesModule.ts
+import { AllianceService, Alliance } from "../../AllianceServices";
 
 export class RulesModule {
-  static validateJoin(allianceId: string, memberId: string) {
+  static async validateJoin(allianceId: string, memberId: string): Promise<void> {
+    const alliance: Alliance | null = await AllianceService.getAlliance(allianceId);
+    if (!alliance) throw new Error(`[RulesModule] Alliance ${allianceId} does not exist`);
+
+    if (alliance.members.includes(memberId)) {
+      throw new Error(`[RulesModule] Member ${memberId} already in alliance ${allianceId}`);
+    }
+
     console.log(`[RulesModule] validateJoin: ${memberId} in ${allianceId}`);
   }
 
-  static validateLeaderChange(allianceId: string, newLeaderId: string) {
+  static async validateLeaderChange(allianceId: string, newLeaderId: string): Promise<void> {
+    const alliance: Alliance | null = await AllianceService.getAlliance(allianceId);
+    if (!alliance) throw new Error(`[RulesModule] Alliance ${allianceId} does not exist`);
+
+    if (!alliance.members.includes(newLeaderId)) {
+      throw new Error(`[RulesModule] New leader ${newLeaderId} is not a member of ${allianceId}`);
+    }
+
     console.log(`[RulesModule] validateLeaderChange: ${newLeaderId} in ${allianceId}`);
   }
 }
