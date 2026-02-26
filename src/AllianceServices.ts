a4cb7b036780db6a6986b2e0e79a040cc3870a42
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 22:04:28 +0100

    Update AllianceServices.ts

--- src/AllianceServices.ts
+++ src/AllianceServices.ts
@@ -1,6 +1,12 @@
 // src/AllianceService.ts
 export class AllianceService {
   static addMember(allianceId: string, memberId: string) {
-    console.log(`[AllianceService] addMember ${memberId} to ${allianceId}`);
+    console.log(`[AllianceService] addMember: ${memberId} to ${allianceId}`);
   }
+
+  static getAlliance(allianceId: string) {
+    return { id: allianceId, members: [] };
+  }
+
+  // inne stuby potrzebne dla pozostałych modułów
 }
