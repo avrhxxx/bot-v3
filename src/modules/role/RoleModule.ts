96eff725824049d53d306c753461e685ddcc7637
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 21:37:11 +0100

    Update RoleModule.ts

--- src/modules/role/RoleModule.ts
+++ src/modules/role/RoleModule.ts
@@ -1,9 +1,7 @@
+// src/modules/role/RoleModule.ts
 export class RoleModule {
   static async assignMemberRoles(allianceId: string, memberId: string) {
-    console.log(`[RoleModule] Assigning roles to ${memberId} in ${allianceId}`);
-  }
-
-  static assignLeaderRole(allianceId: string, memberId: string) {
-    console.log(`[RoleModule] Assigning leader role to ${memberId} in ${allianceId}`);
+    console.log(`[RoleModule] assignMemberRoles ${memberId} in ${allianceId}`);
+    return Promise.resolve();
   }
 }
