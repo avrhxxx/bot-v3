c48ed592aaa883d8391110cf8306089fd936a519
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 22:05:57 +0100

    Update RulesModule.ts

--- src/modules/rules/RulesModule.ts
+++ src/modules/rules/RulesModule.ts
@@ -1,6 +1,12 @@
 // src/modules/rules/RulesModule.ts
+import { AllianceService } from "../../AllianceService";
+
 export class RulesModule {
   static validateJoin(allianceId: string, memberId: string) {
-    console.log(`[RulesModule] validateJoin ${memberId} in ${allianceId}`);
+    console.log(`[RulesModule] validateJoin: ${memberId} in ${allianceId}`);
+  }
+
+  static validateLeaderChange(allianceId: string, newLeaderId: string) {
+    console.log(`[RulesModule] validateLeaderChange: ${newLeaderId} in ${allianceId}`);
   }
 }
