b1bf54bf0e78afb632cbf76026889bbd3c6039a0
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 23:39:31 +0100

    Update AllianceServices.ts

--- src/AllianceServices.ts
+++ src/AllianceServices.ts
@@ -1,8 +1,6 @@
-// src/AllianceServices.ts
-import { MongoClient, Db, Collection } from 'mongodb';
-import { config } from './config';
+import { getCollection } from './mongo/mongoClient';
 
-interface Alliance {
+export interface Alliance {
   id: string;
   name: string;
   members: string[];
@@ -10,36 +8,31 @@ interface Alliance {
 }
 
 export class AllianceService {
-  private static client: MongoClient;
-  private static db: Db;
-  private static alliances: Collection<Alliance>;
-
-  // inicjalizacja po starcie aplikacji
-  static async init() {
-    if (!config.mongoUri) throw new Error('MONGO_URI is not defined!');
-    this.client = new MongoClient(config.mongoUri);
-    await this.client.connect();
-    this.db = this.client.db(); // jeśli nie podałeś nazwy DB w URI, użyje domyślnej
-    this.alliances = this.db.collection<Alliance>('alliances');
-    console.log('[AllianceService] Connected to MongoDB');
+  private static get alliances() {
+    return getCollection<Alliance>('alliances');
   }
 
-  // dodaje członka do sojuszu
-  static async addMember(allianceId: string, memberId: string) {
-    await this.alliances.updateOne(
-      { id: allianceId },
-      { $addToSet: { members: memberId } } // $addToSet dodaje tylko jeśli nie ma jeszcze
-    );
-    console.log(`[AllianceService] addMember: ${memberId} to ${allianceId}`);
-  }
-
-  // inne metody np. tworzenie sojuszu
   static async createAlliance(id: string, name: string) {
     await this.alliances.insertOne({ id, name, members: [] });
     console.log(`[AllianceService] createAlliance: ${id} (${name})`);
   }
 
-  // pobranie sojuszu
+  static async addMember(allianceId: string, memberId: string) {
+    await this.alliances.updateOne(
+      { id: allianceId },
+      { $addToSet: { members: memberId } } // dodaje tylko jeśli brak
+    );
+    console.log(`[AllianceService] addMember: ${memberId} to ${allianceId}`);
+  }
+
+  static async transferLeader(allianceId: string, newLeaderId: string) {
+    await this.alliances.updateOne(
+      { id: allianceId },
+      { $set: { leader: newLeaderId } }
+    );
+    console.log(`[AllianceService] transferLeader: ${newLeaderId} in ${allianceId}`);
+  }
+
   static async getAlliance(allianceId: string) {
     return this.alliances.findOne({ id: allianceId });
   }
