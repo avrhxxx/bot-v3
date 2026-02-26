bbc1235d005a7bd0e18285bd57c71e0f7f745f17
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 21:23:55 +0100

    Update client.ts

--- src/discord/client.ts
+++ src/discord/client.ts
@@ -1,16 +1,6 @@
-export type ClientStub = {
-  on(event: string, listener: (...args: any[]) => void): void;
-  login(): Promise<string>;
-  dispatch(): void;
-  users: { cache: Map<any, any> };
-};
+export type ClientStub = { login(): Promise<string> };
 
 export async function startDiscord(): Promise<ClientStub> {
-  console.log('Discord stub started.');
-  return {
-    on: () => {},
-    login: async () => 'ok',
-    dispatch: () => {},
-    users: { cache: new Map() },
-  };
+  console.log('[Discord] Stub client started');
+  return { login: async () => 'ok' };
 }
