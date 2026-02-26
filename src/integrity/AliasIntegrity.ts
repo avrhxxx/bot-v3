47f15c5752c265f39c7ec795c14b2615ca9e796f
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 22:36:11 +0100

    Update AliasIntegrity.ts

--- src/integrity/AliasIntegrity.ts
+++ src/integrity/AliasIntegrity.ts
@@ -1,4 +1,5 @@
-import { AllianceService } from "../AllianceServices";
+// src/integrity/AliasIntegrity.ts
+import { AllianceService } from "../AllianceServices"; // poprawna ścieżka
 import { MembershipModule } from "../modules/membership/MembershipModule";
 import { RoleModule } from "../modules/role/RoleModule";
 import { ChannelModule } from "../modules/channel/ChannelModule";
@@ -6,5 +7,6 @@ import { ChannelModule } from "../modules/channel/ChannelModule";
 export class AliasIntegrity {
   static checkAlliance(allianceId: string) {
     console.log(`[AliasIntegrity] checkAlliance: ${allianceId}`);
+    // Tutaj można później dodać logikę sprawdzania spójności sojuszu
   }
 }
