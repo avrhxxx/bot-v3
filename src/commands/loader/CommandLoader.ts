701a45127d77cdb052a3a0cad11753f2478789df
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 20:59:03 +0100

    Update CommandLoader.ts

--- src/commands/loader/CommandLoader.ts
+++ src/commands/loader/CommandLoader.ts
@@ -1,16 +1,15 @@
-// src/commands/loader/CommandLoader.ts
 import { CommandRegistry, Command } from '../CommandRegistry';
 
 export class CommandLoader {
   static async loadAllCommands(): Promise<void> {
-    const commands: Command[] = await Promise.resolve([
+    const commands: Command[] = [
       { name: 'ping', execute: () => console.log('pong') },
       { name: 'echo', execute: () => console.log('echo') },
-    ]);
+    ];
 
     CommandRegistry.register(commands);
 
-    CommandRegistry.getAll().forEach((cmd: Command) => {
+    CommandRegistry.getAll().forEach(cmd => {
       console.log(`Command loaded: ${cmd.name}`);
     });
   }
