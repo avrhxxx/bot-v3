d3e085f88c78e19ccb6ae75a468ecb8e65c4d473
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 20:50:23 +0100

    Update Journal.ts

--- src/journal/Journal.ts
+++ src/journal/Journal.ts
@@ -1,14 +1,21 @@
+
 // src/journal/Journal.ts
 import type { JournalEntry } from './JournalTypes';
 
 export class Journal {
-  private entries: JournalEntry[] = [];
+  private static entries: JournalEntry[] = [];
 
-  log(entry: JournalEntry) {
+  static create(entry: JournalEntry) {
     this.entries.push(entry);
   }
 
-  getAll(): JournalEntry[] {
+  static updateStatus(index: number, status: string) {
+    if (this.entries[index]) {
+      this.entries[index].status = status;
+    }
+  }
+
+  static getAll(): JournalEntry[] {
     return this.entries;
   }
 }
