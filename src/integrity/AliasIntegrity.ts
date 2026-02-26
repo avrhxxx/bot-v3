9f9eef8c96b777d9cea305c059116ca04761f39f
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 22:27:45 +0100

    Update AliasIntegrity.ts

--- src/integrity/AliasIntegrity.ts
+++ src/integrity/AliasIntegrity.ts
@@ -1,5 +1,4 @@
-// src/integrity/AliasIntegrity.ts
-import { AllianceService } from "../AllianceService"; // Sprawdź, że plik faktycznie nazywa się AllianceService.ts
+import { AllianceService } from "../AllianceServices";
 import { MembershipModule } from "../modules/membership/MembershipModule";
 import { RoleModule } from "../modules/role/RoleModule";
 import { ChannelModule } from "../modules/channel/ChannelModule";
@@ -7,6 +6,5 @@ import { ChannelModule } from "../modules/channel/ChannelModule";
 export class AliasIntegrity {
   static checkAlliance(allianceId: string) {
     console.log(`[AliasIntegrity] checkAlliance: ${allianceId}`);
-    // Tutaj później można dodać logikę spójności
   }
 }
