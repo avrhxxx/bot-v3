85c6717d8bc2b1e8b6d0a5542ccc41c09b8e7d0c
avrhxxx <stelczyk0797@gmail.com>
2026-02-26 22:05:23 +0100

    Update RoleModule.ts

--- src/modules/role/RoleModule.ts
+++ src/modules/role/RoleModule.ts
@@ -1,7 +1,13 @@
 // src/modules/role/RoleModule.ts
+import { AllianceService } from "../../AllianceService";
+import { MutationGate } from "../../engine/MutationGate";
+
 export class RoleModule {
   static async assignMemberRoles(allianceId: string, memberId: string) {
-    console.log(`[RoleModule] assignMemberRoles ${memberId} in ${allianceId}`);
-    return Promise.resolve();
+    console.log(`[RoleModule] assignMemberRoles: ${memberId} in ${allianceId}`);
+  }
+
+  static assignLeaderRole(allianceId: string, memberId: string) {
+    console.log(`[RoleModule] assignLeaderRole: ${memberId} in ${allianceId}`);
   }
 }
