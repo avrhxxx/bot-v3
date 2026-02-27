ca38697e9506a719e672e5fa3fddf4a56e3c3d89
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:14:43 +0100

    Delete src/config directory

--- src/config/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  mongoUri: process.env.MONGO_URI || '',
-  discordToken: process.env.BOT_TOKEN || '',
-  clientId: process.env.CLIENT_ID || '',
-  guildId: process.env.GUILD_ID || ''
-};
