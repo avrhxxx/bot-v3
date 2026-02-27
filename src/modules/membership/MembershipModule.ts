158223c955255d322475a756bb7db0eadd3f5875
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:16:52 +0100

    Delete src/modules directory

--- src/modules/membership/MembershipModule.ts
+++
@@ -1,8 +0,0 @@
-import { MutationGate, MutationOptions } from '../../engine/MutationGate';
-
-export class MembershipModule {
-    static addMember(memberId: string, allianceId: string, actorId?: string) {
-        const options: MutationOptions = { actorId };
-        MutationGate.execute('ADD_MEMBER', allianceId, memberId, options);
-    }
-}
