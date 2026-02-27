67aeaae39b9554fba1f6c9510afa00ceefbceafb
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 03:57:26 +0100

    Update testIndex.ts

--- src/testIndex.ts
+++ src/testIndex.ts
@@ -1,12 +1,9 @@
-// src/testIndex.ts
 import { Client, GatewayIntentBits, Guild } from "discord.js";
 import { BOT_TOKEN, GUILD_ID } from "./config/config";
 import { RoleModule } from "./modules/role/RoleModule";
 import { ChannelModule } from "./modules/channel/ChannelModule";
 
-// --------------------------
 // Testowy sojusz
-// --------------------------
 const TEST_ALLIANCE_TAG = "TsT";
 const TEST_ALLIANCE_NAME = "TestAlliance";
 const TEST_ALLIANCE_ID = "alliance-test";
@@ -17,7 +14,6 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.once("ready", async () => {
   console.log(`Testowy bot zalogowany jako ${client.user?.tag}`);
 
-  // Pobieramy guild z cache po ID
   const guild: Guild | undefined = client.guilds.cache.get(GUILD_ID);
   if (!guild) {
     console.log(`Nie znaleziono guilda o ID ${GUILD_ID}.`);
@@ -28,7 +24,11 @@ client.once("ready", async () => {
     // --------------------------
     // 1️⃣ Tworzymy role dla testowego sojuszu
     // --------------------------
-    const createdRoles = await RoleModule.createRoles(guild, TEST_ALLIANCE_TAG, TEST_ALLIANCE_NAME);
+    const createdRoles = await RoleModule.createRoles(
+      guild,
+      TEST_ALLIANCE_TAG,
+      TEST_ALLIANCE_NAME
+    );
     console.log("Stworzone role:", createdRoles);
 
     // --------------------------
