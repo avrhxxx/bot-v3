8b13e2aa1c62ab30157b78c506926d6f0fc3a332
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:56:19 +0100

    Update AllianceOrkiestror.ts

--- src/orkiestror/AllianceOrkiestror.ts
+++ src/orkiestror/AllianceOrkiestror.ts
@@ -2,6 +2,8 @@
 import { RoleModule } from "../modules/role/RoleModule";
 import { ChannelModule } from "../modules/channel/ChannelModule";
 import { RulesModule } from "../modules/rules/RulesModule";
+import { MembershipModule } from "../modules/membership/MembershipModule";
+import { TransferLeaderModule } from "../modules/transferleader/TransferLeaderModule";
 
 export class AllianceOrkiestror {
   async setupAllianceStub(guild: any, allianceId: string, tag: string, name: string) {
@@ -10,5 +12,10 @@ export class AllianceOrkiestror {
     await RoleModule.ensureRoles(guild);
     await ChannelModule.createChannels(guild, allianceId, tag, name);
     RulesModule.canPromoteToLeader("stub-member-id");
+
+    await MembershipModule.addMember(allianceId, "stub-member-id");
+    await TransferLeaderModule.transferLeader(allianceId, "stub-member-id");
+
+    console.log("[Orkiestror] wszystkie stuby wywołane");
   }
 }
