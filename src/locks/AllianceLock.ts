1aa0b4374e2a15817a3dc0e32bcff8b31a90d977
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:16:45 +0100

    Delete src/locks directory

--- src/locks/AllianceLock.ts
+++
@@ -1,6 +0,0 @@
-// src/locks/AllianceLock.ts
-export class AllianceLock {
-  static async run<T>(allianceId: string, handler: () => Promise<T> | T): Promise<T> {
-    return handler();
-  }
-}
--- src/locks/GlobalLock.ts
+++
@@ -1,6 +0,0 @@
-// src/locks/GlobalLock.ts
-export class GlobalLock {
-  static async run<T>(handler: () => Promise<T> | T): Promise<T> {
-    return handler();
-  }
-}
