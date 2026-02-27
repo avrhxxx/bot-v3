6ba0032dc85930784028e89687384c8771ff4422
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:04:08 +0100

    Update RulesModule.ts

--- src/modules/rules/RulesModule.ts
+++ src/modules/rules/RulesModule.ts
@@ -2,24 +2,14 @@ import { AllianceOrkiestror } from '../../orkiestror/AllianceOrkiestror';
 
 export class RulesModule {
     static validateJoin(memberId: string, allianceId: string) {
-        const alliance = AllianceOrkiestror.findAlliance(allianceId);
+        const alliance = AllianceOrkiestror.getAlliance(allianceId);
         if (!alliance) throw new Error(`[RulesModule] Alliance ${allianceId} does not exist`);
-
-        if (alliance.members.includes(memberId)) {
-            throw new Error(`[RulesModule] Member ${memberId} is already in alliance ${allianceId}`);
-        }
-
         return true;
     }
 
     static validateLeaderChange(newLeaderId: string, allianceId: string) {
-        const alliance = AllianceOrkiestror.findAlliance(allianceId);
+        const alliance = AllianceOrkiestror.getAlliance(allianceId);
         if (!alliance) throw new Error(`[RulesModule] Alliance ${allianceId} does not exist`);
-
-        if (!alliance.members.includes(newLeaderId)) {
-            throw new Error(`[RulesModule] Member ${newLeaderId} is not part of alliance ${allianceId}`);
-        }
-
         return true;
     }
 }
