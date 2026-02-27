c8b203d44d0279862f5ccaf0c6815057762a9858
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 15:41:03 +0100

    Update syncEngine.ts

--- src/sync/syncEngine.ts
+++ src/sync/syncEngine.ts
@@ -1,1 +1,9 @@
+export class SyncEngine {
+  constructor() {
+    console.log("[SyncEngine] Placeholder initialized");
+  }
 
+  start() {
+    console.log("[SyncEngine] Start method called (placeholder)");
+  }
+}
