dddd7bdb4def002f1f53e54ba77f82113a3191cc
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 15:22:02 +0100

    Create shadowAuthority.ts

---
+++ src/sync/units/shadowAuthority.ts
@@ -0,0 +1,11 @@
+import { Guild, GuildBasedChannel } from "discord.js";
+
+export const ShadowAuthorityUnit = async (
+  guild: Guild,
+  shadowRoleId: string,
+  authorityIds: string[],
+  notifyChannel?: GuildBasedChannel
+) => {
+  console.log("🔹 [ShadowAuthorityUnit] Placeholder task running...");
+  // TODO: tu w przyszłości pełna logika synchronizacji i powiadomień
+};
