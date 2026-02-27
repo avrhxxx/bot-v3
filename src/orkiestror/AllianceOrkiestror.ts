2bf934b2a3369e340719b78ca9e68efdc7e92d76
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:53:44 +0100

    Update AllianceOrkiestror.ts

--- src/orkiestror/AllianceOrkiestror.ts
+++ src/orkiestror/AllianceOrkiestror.ts
@@ -1,33 +1,14 @@
-import { Guild, GuildMember } from "discord.js";
+// src/orkiestror/AllianceOrkiestror.ts
 import { RoleModule } from "../modules/role/RoleModule";
 import { ChannelModule } from "../modules/channel/ChannelModule";
 import { RulesModule } from "../modules/rules/RulesModule";
 
 export class AllianceOrkiestror {
-  private roleModule: RoleModule;
-  private channelModule: ChannelModule;
-  private rulesModule: RulesModule;
+  async setupAllianceStub(guild: any, allianceId: string, tag: string, name: string) {
+    console.log("[Orkiestror] setupAllianceStub wywołane");
 
-  constructor(private guild: Guild) {
-    this.roleModule = new RoleModule(guild);
-    this.channelModule = new ChannelModule(guild);
-    this.rulesModule = new RulesModule();
-  }
-
-  /** Minimalny flow: tworzymy role, kanały i przypisujemy lidera */
-  async setupAlliance(allianceName: string, leader: GuildMember) {
-    // 1️⃣ Tworzymy role
-    await this.roleModule.createRoles();
-
-    // 2️⃣ Tworzymy kategorię i kanały
-    const category = await this.channelModule.createAllianceCategory(allianceName);
-    await this.channelModule.createAllianceChannels(category);
-
-    // 3️⃣ Walidacja i przypisanie lidera
-    if (!this.rulesModule.canPromoteToLeader()) {
-      throw new Error("Alliance already has a leader");
-    }
-    this.rulesModule.addLeader();
-    await this.roleModule.assignRole(leader, "R5 - Leader");
+    await RoleModule.ensureRoles(guild);
+    await ChannelModule.createChannels(guild, allianceId, tag, name);
+    RulesModule.canPromoteToLeader("stub-member-id");
   }
 }
