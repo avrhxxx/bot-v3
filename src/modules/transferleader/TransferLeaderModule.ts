8cf42cd3d914d717e93986ed63f19616606561d9
avrhxxx <stelczyk0797@gmail.com>
2026-02-27 02:02:45 +0100

    Update TransferLeaderModule.ts

--- src/modules/transferleader/TransferLeaderModule.ts
+++ src/modules/transferleader/TransferLeaderModule.ts
@@ -3,11 +3,10 @@ import { MutationGate, MutationOptions } from '../../engine/MutationGate';
 
 export class TransferLeaderModule {
     static transferLeader(allianceId: string, newLeaderId: string, reason?: string) {
-        const alliance = AllianceOrkiestror.findAlliance(allianceId);
+        const alliance = AllianceOrkiestror.getAlliance(allianceId);
         if (!alliance) throw new Error(`[TransferLeaderModule] Alliance ${allianceId} does not exist`);
 
-        // MutationGate oczekuje obiektu typu MutationOptions
-        const options: MutationOptions = { /* przypisz tu pola zgodnie z typem MutationOptions */ };
+        const options: MutationOptions = { reason };
         MutationGate.execute('TRANSFER_LEADER', allianceId, newLeaderId, options);
     }
 }
