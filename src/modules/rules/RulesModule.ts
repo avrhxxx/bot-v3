1ca2fae00eb1264e2dbc521ed31453ce35a186e3
avrhxxx <stelczyk0797@gmail.com>
2026-02-28 15:19:54 +0100

    Delete src/modules/rules/RulesModule.ts

--- src/modules/rules/RulesModule.ts
+++
@@ -1,7 +0,0 @@
-// src/modules/rules/RulesModule.ts
-export class RulesModule {
-  static canPromoteToLeader(_memberId: string) {
-    console.log("[Stub] RulesModule.canPromoteToLeader wywołane");
-    return true; // na razie zawsze true
-  }
-}
