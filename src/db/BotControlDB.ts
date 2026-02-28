e03b3b974bd0ae494a705b613fe030c0b89229ae
avrhxxx <stelczyk0797@gmail.com>
2026-02-28 15:51:33 +0100

    Update BotControlDB.ts

--- src/db/BotControlDB.ts
+++ src/db/BotControlDB.ts
@@ -1,1 +1,36 @@
 
+class BotControlDatabase {
+
+  private controlRoleId: string | null = null;
+  private authorizedUserIds: Set<string> = new Set();
+
+  setControlRole(roleId: string) {
+    this.controlRoleId = roleId;
+  }
+
+  getControlRole(): string | null {
+    return this.controlRoleId;
+  }
+
+  setAuthorizedUsers(userIds: string[]) {
+    this.authorizedUserIds = new Set(userIds);
+  }
+
+  getAuthorizedUsers(): string[] {
+    return Array.from(this.authorizedUserIds);
+  }
+
+  addUser(userId: string) {
+    this.authorizedUserIds.add(userId);
+  }
+
+  removeUser(userId: string) {
+    this.authorizedUserIds.delete(userId);
+  }
+
+  isAuthorized(userId: string): boolean {
+    return this.authorizedUserIds.has(userId);
+  }
+}
+
+export const BotControlDB = new BotControlDatabase();
