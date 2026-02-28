5c6461db4251307dfb0ed08c44f4c7f1e9f54491
avrhxxx <stelczyk0797@gmail.com>
2026-02-28 17:30:52 +0100

    Update EventUnit.ts

--- src/sync/units/EventUnit/EventUnit.ts
+++ src/sync/units/EventUnit/EventUnit.ts
@@ -1,1 +1,22 @@
+import { Guild } from "discord.js";
+import { syncLiveDB } from "../../../db/SyncLiveDB";
+import { allianceDB } from "../../../db/AllianceDB";
+import { DelayModel } from "../../SyncDelayModel";
 
+export class EventUnit {
+  private delayModel: DelayModel;
+  public name = "EventUnit";
+
+  constructor(delayModel: DelayModel) {
+    this.delayModel = delayModel;
+  }
+
+  public async run(guild: Guild) {
+    // działa w czasie rzeczywistym, nie czeka na cykl
+    console.log(`[EventUnit] Listening to events...`);
+
+    // może tymczasowo zamrozić synchronizację
+    // delay model będzie uwzględniał freeze
+    await this.delayModel.waitAction();
+  }
+}
